b3571c376cc4803ce0941625daef5f820d160e75
rowen <rowen@AlbireoAir.local>
2009-03-07 01:38:01 +0000

    Use auto-name detection in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ python setup.py install
 import sdss3tools
 
 sdss3tools.setup(
-    name = "actorkeys",
     description = "Command and keyword data dictionaries for actors in the SDSS-III operations system",
 )
