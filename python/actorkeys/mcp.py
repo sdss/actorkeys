01b3ebf1f53a1991ff836a7c8dadb97ba298b528
rowen <rowen@AlbireoAir.local>
2009-04-10 21:16:43 +0000

    Further tweaks to MCP dictionary. The sp1/2Slithead door order is now correct.

--- python/actorkeys/mcp.py
+++ python/actorkeys/mcp.py
@@ -1,5 +1,5 @@
 KeysDictionary("mcp", (1,1),
-    # command-related keywords
+    # Command-related keywords
     Key("badAxis", String(), doCache=False, help="Unknown telescope axis"),
     Key("badCharacter", Int(), doCache=False, help="ASCII code of invalid character"),
     Key("badCommand", String(), doCache=False, help="Invalid command string"),
@@ -13,13 +13,13 @@ KeysDictionary("mcp", (1,1),
     Key("userId", String(), help="Name reported by user"),
     Key("yourUserNum", Int(), help="Your user ID number"),
 
-    # iack and semaphore
+    # Iack and semaphore
     Key("haveSemaphore", Bool("false", "true"), help="Do you have the semaphore?"),
     Key("needIack", Bool("false", "true"), help="If False, please send an iack"),
     Key("needSemaphore", help="Command rejected because you don't have the semaphore"),
     Key("semaphoreOwner", String(), help="Name of user (self-reported) who owns the semaphore"),
     
-    # flat field screens
+    # Flat field screens
     Key("ffsCloseFailed", Bool("false", "true"), doCache=False),
     Key("ffsCommanded", Bool("false", "true"), doCache=False),
     Key("ffsCommandedOn", Bool("false", "true"), help="Flat field petals commanded close/open"),
@@ -30,7 +30,7 @@ KeysDictionary("mcp", (1,1),
     Key("ffsStatus", Enum("00", "01", "10", "11", descr=("?", "Closed", "Open", "Invalid"))*8,
         help="State of flat field petals 1-8"),
 
-    # lamps
+    # Lamps
     Key("ffLamp", Bool("0", "1")*4, help="Detected state of flat field lamps"),
     Key("ffLampCommandedOn", Bool("false", "true"), help="Commanded state of flat-field lamps"),
     Key("hgCdLamp", Bool("0", "1")*4, help="Detected state of mercury/cadmium lamps"),
@@ -40,22 +40,28 @@ KeysDictionary("mcp", (1,1),
     Key("uvLampCommandedOn", Bool("false", "true"), help="Commanded state of UV lamps"),
     Key("whtLampCommandedOn", Bool("false", "true"), help="Commanded state of white lamps"),
 
-    # instrument ID switches
+    # Counterweights
+    Key("cwAbort", Int(), doCache=False, help="Abort on counterweight 1,2,3 or 4 (only one abort is reported at a time)"),
+    Key("cwPositions", Int()*4, help="Counterweight positions"),
+    Key("cwStatus", Enum("..", "L.", ".U", "LU", descr=("InRange", "AtLowerLimit", "AtUpperLimit", "Invalid"))*4,
+        help="Counterweight status"),
+
+    # Other PLC signals
+    Key("altWindscreenTouched", Enum("00", "01", "10", "11", descr=("None", "Down", "Up", "Both"))),
+    Key("azWindscreenTouched", Enum("00", "01", "10", "11", descr=("None", "CW", "CCW", "Both"))),
     Key("instrumentNum", Int(invalid="-1"),
         help="Instrument ID; 0=no instrument; 14=imager; -1=switches inconsistent or could not get semaphore"),
     Key("instrumentNumConsistent", Bool("false", "true"),
         help="Do the three instrument ID switches agree? If not, instrumentNumValues is also output."),
     Key("instrumentNumValues", Int()*3, help="Reading from each instrument ID switch"),
+    Key("saddleIsMounted", Bool("false", "true"), help="Imager saddle is mounted"),
+    Key("sp1Slithead", Bool("0", "1", help="Slithead door open"), Bool("0", "1", help="Slithead door closed"),
+        Bool("0", "1", help="Latch extended"), Bool("0", "1", help="Slithead in place")),
+    Key("sp2Slithead", Bool("0", "1", help="Slithead door open"), Bool("0", "1", help="Slithead door closed"),
+        Bool("0", "1", help="Latch extended"), Bool("0", "1", help="Slithead in place")),
+    Key("tbarCommanded", Bool("false", "true"), doCache=False, help="Imager TBar move requested"),
 
-    # versions
-    Key("goodFiducialVersions", Bool("false", "true"), help="The fiducial table version numbers are consistent"),
-    Key("azFiducialVersion", String(), help="Version of fiducial table"),
-    Key("altFiducialVersion", String(), help="Version of fiducial table"),
-    Key("rotFiducialVersion", String(), help="Version of fiducial table"),
-    Key("mcpVersion", String(), help="Version of MCP code"),
-    Key("plcVersions", Int()*2, help="The PLC versions from the Allen-Bradley and data_collection.h"),
-
-    # axis controller -- in order Az, Alt, Rot
+    # Axis controller, in order Az, Alt, Rot
     Key("azMaxAccLimit", Float(units="deg/s^2"), help="Maximum acceleration allowed"),
     Key("azMaxAccRequested", Float(units="deg/s^2"), doCache=False, help="Maximum acceleration requested"),
     Key("azMaxVelLimit", Float(units="deg/s"), help="Maximum velocity allowed"),
@@ -86,25 +92,15 @@ KeysDictionary("mcp", (1,1),
         Int(help="DAC limit"), Int(help="Shift"), Int(help="Friction FF"),
         help="Servo loop tuning parameters; for details see the MEI manual"),
 
-    # windscreen
-    Key("altWindscreenTouched", Enum("00", "01", "10", "11", descr=("None", "Down", "Up", "Both"))),
-    Key("azWindscreenTouched", Enum("00", "01", "10", "11", descr=("None", "CW", "CCW", "Both"))),
-
-    # counterweights
-    Key("cwAbort", Int(), doCache=False, help="Abort on counterweight 1,2,3 or 4 (only one abort is reported at a time)"),
-    Key("cwPositions", Int()*4, help="Counterweight positions"),
-    Key("cwStatus", Enum("..", "L.", ".U", "LU", descr=("InRange", "AtLowerLimit", "AtUpperLimit", "Invalid"))*4,
-        help="Counterweight status"),
-
-    # encoders and fiducials
+    # Axis encoders and fiducials
     Key("minEncoderMismatch", Int()*3, help="Limit below which readings from paired encoders are considered equal (az, alt, rot)"),
     Key("msOnMaxCorrection", Int()*3, help="Maximum fiducial error which will be corrected (az, alt, rot)"),
 
-    # miscellaneous PCL
-    Key("saddleIsMounted", Bool("false", "true"), help="Imager saddle is mounted"),
-    Key("sp1Slithead", Bool("0", "1", help="?Door closed?"), Bool("0", "1", help="?Door open?"),
-        Bool("0", "1", help="Latch extended"), Bool("0", "1", help="Slithead in place")),
-    Key("sp2Slithead", Bool("0", "1", help="?Door closed?"), Bool("0", "1", help="?Door open?"),
-        Bool("0", "1", help="Latch extended"), Bool("0", "1", help="Slithead in place")),
-    Key("tbarCommanded", Bool("false", "true"), doCache=False, help="Imager TBar move requested"),
+    # Versions
+    Key("goodFiducialVersions", Bool("false", "true"), help="The fiducial table version numbers are consistent"),
+    Key("azFiducialVersion", String(), help="Version of fiducial table"),
+    Key("altFiducialVersion", String(), help="Version of fiducial table"),
+    Key("rotFiducialVersion", String(), help="Version of fiducial table"),
+    Key("mcpVersion", String(), help="Version of MCP code"),
+    Key("plcVersions", Int()*2, help="The PLC versions from the Allen-Bradley and data_collection.h"),
 )
