96130949327b035ba6acbfd073e6cd1b3596c743
rowen <rowen@AlbireoAir.local>
2009-07-18 15:49:31 +0000

    python/actorkeys/perms.py - Some keywords had a repeat of (1,None) instead of (0,None)

--- python/actorkeys/perms.py
+++ python/actorkeys/perms.py
@@ -1,8 +1,8 @@
 # Based on what the TCC knows about the perms widget
 KeysDictionary('perms',(1,1),
-    Key('actors', String()*(1,), help="Actors controlled by perms"),
+    Key('actors', String()*(0,), help="Actors controlled by perms"),
     # do not refresh authList
     Key('authList', String()*(1,), help="Program and 0 or more authorized actors"),
-    Key('lockedActors', String()*(1,), help="Actors locked out by APO"),
-    Key('programs', String()*(1,), help="Programs registered with perms"),
+    Key('lockedActors', String()*(0,), help="Actors locked out by APO"),
+    Key('programs', String()*(0,), help="Programs registered with perms"),
 )
