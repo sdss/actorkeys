c38a89ccbbf7212948099f15680adf2b196a7786
rhl <rhl@AlbireoAir.local>
2009-07-19 23:37:06 +0000

    Only run make if plc is setup

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@
 To use:
 python setup.py install
 """
-import sys, subprocess
+import os, sys, subprocess
 import sdss3tools
 
 if len(sys.argv) > 1 and sys.argv[1] in ("build", "install"):
-    subprocess.call(["make"], cwd="python/actorkeys")
+    if os.environ.get("PLC_DIR"):
+        subprocess.call(["make"], cwd="python/actorkeys")
 
 sdss3tools.setup(
     description = "Command and keyword data dictionaries for actors in the SDSS-III operations system",
