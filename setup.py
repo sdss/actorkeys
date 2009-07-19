8f0e7ae17ee1b872d0d6fbc04b16adc28f9fba3f
rhl <rhl@AlbireoAir.local>
2009-07-19 18:33:23 +0000

    Machine generate mcp.py from PLC bits and mcp.py.skl

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@
 To use:
 python setup.py install
 """
+import sys, subprocess
 import sdss3tools
+
+if len(sys.argv) > 1 and sys.argv[1] in ("build", "install"):
+    subprocess.call(["make"], cwd="python/actorkeys")
 
 sdss3tools.setup(
     description = "Command and keyword data dictionaries for actors in the SDSS-III operations system",
-)
+    )
