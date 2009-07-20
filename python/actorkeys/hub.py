e42a726bb46fd46f737e3a7f3137a467bb698cc4
rowen <rowen@AlbireoAir.local>
2009-07-20 18:27:01 +0000

    Minor tweak to doCache.

--- python/actorkeys/hub.py
+++ python/actorkeys/hub.py
@@ -1,8 +1,7 @@
 # Based on what the TCC knows about the perms widget
-KeysDictionary('hub',(1,4),
+KeysDictionary('hub',(1,5),
     Key('actors', String()*(0,), help="Current actors"),
     Key('commanders', String()*(0,), help="Current commanders"),
-    # do NOT refresh user:
     Key('user', String()*(5,6), help=
         """Information about a user:
         - cmdrID (program.name)
@@ -12,8 +11,9 @@ KeysDictionary('hub',(1,4),
         - IP address (numeric)
         ? fully qualified domain name (if supplied)
         """,
+        doCache=False,
     ),
-    Key('text', String(), help="Craig's random musings"),
+    Key('text', String(), help="Craig's random musings", doCache=False),
     Key('users', String()*(0,), help="current human (non-hub) commanders"),
     Key('version', String(), help="Craig's random choice of version string"),
 )
