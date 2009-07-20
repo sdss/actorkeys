3a7cd342e1728503d3d8b398f69fc9e4b36dff51
rhl <rhl@AlbireoAir.local>
2009-07-20 17:00:52 +0000

    Added keyDictionaries to list all known dictionaries

--- python/actorkeys/__init__.py
+++ python/actorkeys/__init__.py
@@ -2,4 +2,9 @@
 SDSS-3 Actor keyword dictionaries
 
 Refer to https://trac.sdss3.org/wiki/Ops/KeysDictionary for details.
+
+Please add your dictionary to the keyDictionaries list if you want it
+automatically loaded by e.g. the critical errors actor
 """
+
+keyDictionaries = ("boss", "cmds", "gcam", "hub", "mcp", "msg", "perms", "tcc")
