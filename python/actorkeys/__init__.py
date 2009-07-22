8f4c9638d36758b5a9dcacc1ff0d6d28947f4bd2
rowen <rowen@AlbireoAir.local>
2009-07-22 21:55:14 +0000

    Fixed auto-detect.

--- python/actorkeys/__init__.py
+++ python/actorkeys/__init__.py
@@ -3,13 +3,16 @@ SDSS-3 Actor keyword dictionaries
 
 Refer to https://trac.sdss3.org/wiki/Ops/KeysDictionary for details.
 """
+import os
 import glob
 
 def _getStdDictionaryNames():
+    currPath = os.path.dirname(__file__)
     nonStdDicts = ("__init__.py", "testing.py")
-    allModules = glob.glob("*.py")
+    allModules = glob.glob(os.path.join(currPath, "*.py"))
     retList = []
-    for modName in allModules:
+    for modPath in allModules:
+        modName = os.path.basename(modPath)
         if "_" in modName:
             continue
         if " " in modName:
