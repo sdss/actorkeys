140a83efce0a7b27c4eb135855ef42d688096d40
rhl <rhl@AlbireoAir.local>
2010-05-01 22:52:43 +0000

    Fix dictionary entry for bypassed

--- python/actorkeys/sop.py
+++ python/actorkeys/sop.py
@@ -1,5 +1,17 @@
-KeysDictionary("sop", (1,1),
+KeysDictionary("sop", (1,2),
     # misc
     Key("version", String(help="EUPS/SVN version")),
     Key("text", String(), help="text for humans"),
+    Key("bypassed",
+        #CompoundValueType(String(), Bool("False", "True"))*9,
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        CompoundValueType(String(), Bool("False", "True")),
+        help="Which subsystems's failures are being ignored"),
 )
