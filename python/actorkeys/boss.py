061c75ad15cc4da1f6ab0d051cafdfd8d852217c
djmargala <djmargala@AlbireoAir.local>
2010-09-21 21:26:39 +0000

    added ion pump pressure keywords to dict

--- python/actorkeys/boss.py
+++ python/actorkeys/boss.py
@@ -18,7 +18,7 @@ KeysDictionary("boss",(2,12),*(
         Float(units="mjd",strFormat="%.2f"),
         help = "Time that an exposure began."
     ),
-    Key('EndExposure'cd ..,
+    Key('EndExposure',
         UInt(),
         Float(units = "mjd", strFormat = "%.2f"),
         help = "Time that an exposure ended."
