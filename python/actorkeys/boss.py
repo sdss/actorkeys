f3c73e535404f9f3cbdf638046bd4ee44e8c51be
dkirkby <dkirkby@AlbireoAir.local>
2010-09-22 21:01:08 +0000

    bump boss dictionary version to 2.13

--- python/actorkeys/boss.py
+++ python/actorkeys/boss.py
@@ -1,4 +1,4 @@
-KeysDictionary("boss",(2,12),*(
+KeysDictionary("boss",(2,13),*(
     # misc
     Key("text", String(), help="text for humans"),
     Key("version", String(), help="version string derived from svn info."),
