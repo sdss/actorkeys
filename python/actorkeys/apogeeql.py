51ecba7dd0f859938891e775d24ab13c4f576327
rowen <rowen@AlbireoAir.local>
2011-05-06 20:41:16 +0000

    Added snrGoal keyword. Tweaked names and descriptions of other keyword fields accordingly.

--- python/actorkeys/apogeeql.py
+++ python/actorkeys/apogeeql.py
@@ -30,20 +30,20 @@ KeysDictionary("apogeeql",(0,1),*(
     # one for each exposure taken with the current plate
     Key('exposureData',
         Int(name='plateId', help='Plate ID number'),
+        Int(name='expNum', help='Exposure number for this plate'),
         String(name='expName', help='Exposure name'),
-        String(name='expName', help='Exposure Name'),
-        Float(name='exptime', help='Exposure Time', units='seconds'),
+        Float(name='exptime', help='Exposure time', units='seconds'),
         Float(name='numReads', help='Number of UTR Reads'),
-        Float(name='snrGoal', help='The target SNR for this exposure.'),
+        Float(name='snrGoal', help='The target S/N for this exposure.'),
         Float(name='ditherPos', help='dither Position', units='pixels'),
-        Float(name='snrH12', help='SNR obtained for H=12.0 for this exposure'),
+        Float(name='snr', help='S/N obtained (at reference H mag) for this exposure'),
         Float(name='netExptime', help='Total exposure time for this plate', units='seconds'),
-        Float(name='netSnrH12', help='Cumulative SNR obtained for H=12.0 for this plate')),
+        Float(name='netSnr', help='Cumulative S/N (at reference H mag) for this plate')),
 
     # (S/N)^2 at H=12.0 vs Time Data Keywords (broadcasted with status or when it changes)
     Key('snrAxisRange',
         Float()*2,
-        help='Low, High limits of the Y-Axis (SNR) plot (broadcasted once)'),
+        help='Low, High limits of the Y-Axis (S/N) plot (broadcasted once)'),
     
     Key('snrGoal',
         Float(name='snrGoal', help='Target S/N for specified H magnitude'),
@@ -54,9 +54,9 @@ KeysDictionary("apogeeql",(0,1),*(
     Key('utrData',
         String(name='expName', help='Exposure name'),
         Int(name='readNum', help='Read number counter'),
-        Float(name='snrH12', help='SNR value for this read'),
-        Float(name='snrTotalLinFit', help='SNR^2 to readNum Linear fit to all the reads so far: y intercept, slope')*2,
-        Float(name='snrRecentLinFit', help='SNR^2 to readNum Linear fit of most recent reads: y intercept, slope')*2,
+        Float(name='snr', help='S/N value (at reference H mag) for this read'),
+        Float(name='snrTotalLinFit', help='(S/N)^2 to readNum Linear fit to all the reads so far: y intercept, slope')*2,
+        Float(name='snrRecentLinFit', help='(S/N)^2 to readNum Linear fit of most recent reads: y intercept, slope')*2,
         Bits('fitsBad', 'ditherBad', 'skyBad', 'waveBad', help='bitwise status (1=bad, 0=OK)'),
         Float(name='measDitherPos', help='Measured dither position'),
         Float(name='cmdDitherPos', help='Commanded dither position'),
@@ -64,7 +64,7 @@ KeysDictionary("apogeeql",(0,1),*(
         Float(name='exptimeEst', help='Estimated exposure time to reach snrGoal'),
         Float(name='numReadsToTarget', help='Estimated number of UTR reads to reach snrGoal'),
         Int(name='nReads', help='Total number of UTR reads requested'),
-        Float(name='deltaSNRH12', help="Change in SNR from previous read"),
+        Float(name='deltaSNR', help="Change in S/N (at reference H mag) from previous read"),
         help='Data about the most recent up-the-ramp read'),
 
 ))
