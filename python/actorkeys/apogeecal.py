6489f7bbf3d2ef88ada8c7666a28935b2868ea2e
rowen <rowen@AlbireoAir.local>
2011-05-16 20:15:57 +0000

    Cleaned up the apogeecal dictionary

--- python/actorkeys/apogeecal.py
+++ python/actorkeys/apogeecal.py
@@ -1,22 +1,21 @@
 # This is the initial test keys dictionary for APOGEE Calibration Box
 #
-KeysDictionary("apogeecal", (1,0), *(
+KeysDictionary("apogeecal", (2,1), *(
     # Misc
     Key("text", String(), help="text for humans"),
     Key("version", String(), help="version string derived from svn info."),
 
     # Lamps 
     Key("calSourceNames",
-        String()*(3,10),
-        help="List of calibartion names"),
+        String()*(3,6),
+        help="List of calibration lamp names"),
     Key("calSourceStatus",
-        Bool("false", "true", invalid="?")*3,
-        help="Lamp ON status"),
+        Bool("false", "true", invalid="?")*(3,6),
+        help="Calibration lamp on (true) or off (false)"),
     Key("calShutter",
-        String(),
-        help="State of the CalBox shutter (open | closed)"),
+        Bool("closed", "open", invalid="?"),
+        help="State of the CalBox shutter"),
     Key("calBoxController",
-        String(),
-        help="State of the CalBox controller (on | off)"),
-
+        Bool("off", "on", invalid="?"),
+        help="State of the CalBox controller"),
 ))
