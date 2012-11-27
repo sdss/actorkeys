45e13b72fe6778551a17c2c39abdc4b9e7b983b5
parejkoj <parejkoj@AlbireoAir.local>
2012-11-27 05:00:49 +0000

    Added missing hub keywords

--- python/actorkeys/hub.py
+++ python/actorkeys/hub.py
@@ -17,4 +17,7 @@ KeysDictionary('hub',(1,7),
     Key('users', String()*(0,), help="current human (non-hub) commanders"),
     Key('version', String(), help="Craig's random choice of version string"),
     Key('httpRoot', String()*2, help="hostname and root directory for images"),
+    Key('CmdIn', String(), help="Who sent the command?"),
+    Key('NoTarget', String(), help="Which command target was not found?"),
+    Key('hubtxt', String()),
 )
