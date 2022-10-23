6b588e7b6332483cd65530bc451546fe0a5af363
José Sánchez-Gallego <gallegoj@uw.edu>
2022-10-23 05:38:13 +0000

    Make yao alerts integers

--- python/actorkeys/yao.py
+++ python/actorkeys/yao.py
@@ -404,8 +404,8 @@ KeysDictionary(
     Key("power_supply_volts", Float()),
     Key("filename", String()),
     Key("alive_at", Float(), help="UNIX time of the last time the actor was alive"),
-    Key("b2_ccd_temp_alert", Bool("F", "T"), help="Alert on b2 CCD temperature"),
-    Key("r2_ccd_temp_alert", Bool("F", "T"), help="Alert on r2 CCD temperature"),
-    Key("b2_ln2_temp_alert", Bool("F", "T"), help="Alert on b2 LN2 temperature"),
-    Key("r2_ln2_temp_alert", Bool("F", "T"), help="Alert on r2 LN2 temperature"),
+    Key("b2_ccd_temp_alert", Int(), help="Alert on b2 CCD temperature"),
+    Key("r2_ccd_temp_alert", Int(), help="Alert on r2 CCD temperature"),
+    Key("b2_ln2_temp_alert", Int(), help="Alert on b2 LN2 temperature"),
+    Key("r2_ln2_temp_alert", Int(), help="Alert on r2 LN2 temperature"),
 )
